aa8c3522e10b6caa630a79e1f4bb29ea940a3bba
Tzell <36787074+Tzelal@users.noreply.github.com>
2022-02-17 10:59:02 +0100

    Add files via upload

---
+++ funcLib.c
@@ -0,0 +1,21 @@
+#include <funcLib.h>
+
+void funcLib()
+{
+    volatile uint16_t senOneFlag = false;
+    volatile uint16_t senTwoFlag = false;
+    volatile uint16_t senThreeFlag = false;
+    volatile uint16_t rstOneFlag = false;
+    volatile uint16_t rstTwoFlag = false;
+    volatile uint16_t rstThreeFlag = false;
+
+    char volta0[] = "  A0: ";
+    char volta1[] = "  A1: ";
+    char volta2[] = "  A2: ";
+    char bars[] = " bars. ";
+    char dot[] = ".";
+    char zerro[] = "0";
+    char rst[] = "  -----------RESET-----------\n";
+    char line[] = "  ---------------------------\n";
+    char newline[] = " \r\n";
+}
---
+++ funcLib.h
@@ -0,0 +1,8 @@
+#include <msp430.h>
+
+#ifndef FUNCLIB_H_INCLUDED
+#define FUNCLIB_H_INCLUDED
+
+void funcLib(void);
+
+#endif // FUNCLIB_H_INCLUDED
